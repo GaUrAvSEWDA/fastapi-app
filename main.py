025bd1c6d78bac085c17129e22abd33b1950b4c5
GaUrAvSEWDA <98963810+GaUrAvSEWDA@users.noreply.github.com>
2024-11-10 17:33:52 +0530

    user management api's added

--- main.py
+++ main.py
@@ -127,4 +127,57 @@ async def update_merchant(merchant_id: str, merchant: Merchant):
             merchants[i] = merchant
             return merchant
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Merchant not found")
+
+
+#for user management
+
+class User(BaseModel):
+    user_id: Optional[int] = None  # Default value set to None for optional fields
+    user_name: Optional[str] = None
+    user_role: Optional[str] = None
+    company: Optional[str] = None
+    date_added: str = datetime.now().strftime("%Y-%m-%d")  # This keeps the date in the format YYYY-MM-DD
+    permissions: Optional[List[str]] = None  # Default is None instead of mutable default lis
+
+users = [
+    User(
+        user_id =1 ,
+        user_name = "Ravi",
+        user_role = "Super Admin",
+        company = "NPCI",
+        date_added = "22-10-2023"
+    ),
+    User(
+        user_id = 2 ,
+        user_name = "Raman",
+        user_role = "Admin",
+        company = "HDFC",
+        date_added = "22-10-2023"
+    ),
+]
+
+#get users 
+@app.get("/users", response_model=List[User])
+async def get_users():
+    return users
+#create user
+@app.post("/users", response_model=User, status_code=status.HTTP_201_CREATED)
+async def create_user(user: User):
+    user.user_id = len(users) + 1  # auto-incrementing ID for simplicity
+    users.append(user)
+    return user
+
+@app.put("/users/{user_id}", response_model=User)
+async def update_user_role(user_id: int, new_role: str):
+    for user in users:
+        if user.user_id == user_id:
+            user.user_role = new_role
+            return user
+    raise HTTPException(status_code=404, detail="User not found")
+
+@app.delete("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(user_id: int):
+    global users
+    users = [user for user in users if user.user_id!= user_id]
+    return
     
