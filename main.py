1f00c66359f3ed11ed2cf1021322cd656c9afe8a
GaUrAvSEWDA <98963810+GaUrAvSEWDA@users.noreply.github.com>
2024-11-10 16:00:48 +0530

    delete merchant added

--- main.py
+++ main.py
@@ -110,3 +110,12 @@ async def create_merchant(merchant: Merchant):
     merchants.append(merchant)
     return merchant
     
+#delete merchant
+@app.delete("/merchants/{merchant_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_merchant(merchant_id : int):
+    for merchant in merchants :
+        if merchant.merchant_id == merchant_id:
+            merchants.remove(merchant)
+            return{"message" : "Merchant deleted successfully"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Merchant not found")
+    
